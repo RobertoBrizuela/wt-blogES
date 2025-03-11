68c5c41ca09dc2979653e429dc82e60fccd7edc8
edbrizuela <brizuela.edgar@outlook.com>
2025-03-11 00:34:30 -0600

    Initial: Water treatment website with RO section

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Tratamiento de Aguas';
+export const SITE_DESCRIPTION = 'Soluciones integrales y tecnología avanzada para el tratamiento de agua potable, residual e industrial.';
